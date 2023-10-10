83392a82ea787959cd5ef7beabcde3b8dcf106db
Jack <jack.green@hazelcast.com>
2023-10-10 23:20:55 +0100

    Address query

--- hazelcast/src/main/java/com/hazelcast/config/Config.java
+++ hazelcast/src/main/java/com/hazelcast/config/Config.java
@@ -3247,6 +3247,7 @@ public class Config {
     /** @since 5.4 */
     public Map<String, NamespaceConfig> getNamespaceConfigs() {
         // TODO probably immutable deep copy?
+        // Jack - I don't think so, com.hazelcast.config.Config.getPNCounterConfigs() doesn't do that.
         return Collections.unmodifiableMap(namespaceConfigs);
     }
 
